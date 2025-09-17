3df2682554987ff949a9a9c60f61c578f8ffb159
namnl <>
2025-09-17 19:25:13 +0700

    init app

---
+++ learn-swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  learn-swiftui
+//
+//  Created by Mac mini on 17/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ learn-swiftui/learn_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_swiftuiApp.swift
+//  learn-swiftui
+//
+//  Created by Mac mini on 17/9/25.
+//
+
+import SwiftUI
+
+@main
+struct learn_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
